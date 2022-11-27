16f05195280a13315f89168430e92eaba23bf923
JoaoDias <joaodiasworking@gmail.com>
2022-11-27 17:47:52 -0300

    Finalizando projeto, mas conexão com firebase não esta funcionando de acordo, depois analisar o que esta causando este problema de conexão.

--- next-crud-base/tailwind.config.js
+++ next-crud-base/tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    {
+      pattern:
+        /(bg|from|to)-(red|green|blue|gray)-(50|100|200|300|400|500|600|700|800|900)/,
+    },
+  ],
   theme: {
     extend: {},
   },
